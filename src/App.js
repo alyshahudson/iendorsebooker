e82e015fc157da7746616212a142ad5ffba76b15
Keyairra Wright <keyairra.wright@gmail.com>
2020-05-25 15:25:08 -0400

    Resolve a11y warnings regarding alternative text for images

--- src/App.js
+++ src/App.js
@@ -6,229 +6,229 @@ import './App.scss';
 import ImageGenerator from './ImageGenerator';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      location: '',
-      file: null,
-      blurb: '',
-      bgPhoto: '',
-      bgColor: '',
-      showGeneratedImage: false,
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.readFile = this.readFile.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.toggleImage = this.toggleImage.bind(this);
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			name: '',
+			location: '',
+			file: null,
+			blurb: '',
+			bgPhoto: '',
+			bgColor: '',
+			showGeneratedImage: false,
+		};
+		this.handleChange = this.handleChange.bind(this);
+		this.readFile = this.readFile.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.toggleImage = this.toggleImage.bind(this);
+	}
 
-  handleChange(e) {
-    console.log(e.target.value);
-    this.setState({
-      ...this.state,
-      [e.target.name]: e.target.value,
-    });
-  }
+	handleChange(e) {
+		console.log(e.target.value);
+		this.setState({
+			...this.state,
+			[e.target.name]: e.target.value,
+		});
+	}
 
-  handleSubmit(e) {
-    e.preventDefault();
-    console.log(this.state);
-    this.toggleImage();
-  }
+	handleSubmit(e) {
+		e.preventDefault();
+		console.log(this.state);
+		this.toggleImage();
+	}
 
-  toggleImage() {
-    if (this.state.showGeneratedImage === false) {
-      this.setState({
-        showGeneratedImage: true,
-      });
-    }
-  }
+	toggleImage() {
+		if (this.state.showGeneratedImage === false) {
+			this.setState({
+				showGeneratedImage: true,
+			});
+		}
+	}
 
-  readFile(e) {
-    if (e.target.files[0]) {
-      this.setState({
-        ...this.state,
-        file: URL.createObjectURL(e.target.files[0]),
-      });
-    }
-  }
+	readFile(e) {
+		if (e.target.files[0]) {
+			this.setState({
+				...this.state,
+				file: URL.createObjectURL(e.target.files[0]),
+			});
+		}
+	}
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <section>
-            <article className="textual">
-              <h1>I Endorse BOOKER</h1>
-              <p>
-                Create your own endorsement graphic for Bernie and other
-                progressive candidates to share over Instagram, Twitter,
-                Facebook, emails, and so forth!
-              </p>
-            </article>
-          </section>
-        </header>
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<section>
+						<article className="textual">
+							<h1>I Endorse BOOKER</h1>
+							<p>
+								Create your own endorsement graphic for Bernie and other
+								progressive candidates to share over Instagram, Twitter,
+								Facebook, emails, and so forth!
+							</p>
+						</article>
+					</section>
+				</header>
 
-        <section className="body">
-          <article className="textual">
-            <div className="left-group">
-              <form onSubmit={this.handleSubmit}>
-                {/* name */}
-                <div className="input-group standard">
-                  <input
-                    type="text"
-                    name="name"
-                    onChange={this.handleChange}
-                    placeholder="Enter Full Name"
-                    value={this.state.name}
-                  />
-                  <label htmlFor="name">Name</label>
-                </div>
+				<section className="body">
+					<article className="textual">
+						<div className="left-group">
+							<form onSubmit={this.handleSubmit}>
+								{/* name */}
+								<div className="input-group standard">
+									<input
+										type="text"
+										name="name"
+										onChange={this.handleChange}
+										placeholder="Enter Full Name"
+										value={this.state.name}
+									/>
+									<label htmlFor="name">Name</label>
+								</div>
 
-                {/* location */}
-                <div className="input-group standard">
-                  <input
-                    type="text"
-                    name="location"
-                    onChange={this.handleChange}
-                    placeholder="Enter Location"
-                    value={this.state.location}
-                  />
-                  <label htmlFor="location">Job Title/Location</label>
-                </div>
+								{/* location */}
+								<div className="input-group standard">
+									<input
+										type="text"
+										name="location"
+										onChange={this.handleChange}
+										placeholder="Enter Location"
+										value={this.state.location}
+									/>
+									<label htmlFor="location">Job Title/Location</label>
+								</div>
 
-                {/* why */}
-                <div className="input-group standard">
-                  <textarea
-                    onChange={this.handleChange}
-                    name="blurb"
-                    placeholder="Your text here"
-                    rows="6"
-                    value={this.state.blurb}
-                  />
-                  <label htmlFor="blurb">
-                    Why do you endorse Charles Booker?
-                  </label>
-                </div>
+								{/* why */}
+								<div className="input-group standard">
+									<textarea
+										onChange={this.handleChange}
+										name="blurb"
+										placeholder="Your text here"
+										rows="6"
+										value={this.state.blurb}
+									/>
+									<label htmlFor="blurb">
+										Why do you endorse Charles Booker?
+									</label>
+								</div>
 
-                {/* photo upload */}
-                <div className="input-group non-text">
-                  <input
-                    type="file"
-                    id="file"
-                    accept="image/*"
-                    onChange={this.readFile}
-                  />
-                  <label htmlFor="inputImg">Upload photo</label>
-                  <img id="output" src={this.state.file} alt="" />
-                </div>
+								{/* photo upload */}
+								<div className="input-group non-text">
+									<input
+										type="file"
+										id="file"
+										accept="image/*"
+										onChange={this.readFile}
+									/>
+									<label htmlFor="inputImg">Upload photo</label>
+									<img id="output" src={this.state.file} alt="" />
+								</div>
 
-                {/* colors */}
-                <div className="input-group non-text colors">
-                  <input
-                    type="radio"
-                    name="bgColor"
-                    id="redBG"
-                    value="redBg"
-                    checked={true}
-                    onChange={this.handleChange}
-                  />
-                  <input
-                    type="radio"
-                    name="bgColor"
-                    id="orangeBG"
-                    value="orangeBg"
-                    onChange={this.handleChange}
-                  />
-                  <input
-                    type="radio"
-                    name="bgColor"
-                    id="yellowBG"
-                    value="yellowBg"
-                    onChange={this.handleChange}
-                  />
-                  <label htmlFor="bgColor">Color</label>
-                </div>
+								{/* colors */}
+								<div className="input-group non-text colors">
+									<input
+										type="radio"
+										name="bgColor"
+										id="redBG"
+										value="redBg"
+										checked={true}
+										onChange={this.handleChange}
+									/>
+									<input
+										type="radio"
+										name="bgColor"
+										id="orangeBG"
+										value="orangeBg"
+										onChange={this.handleChange}
+									/>
+									<input
+										type="radio"
+										name="bgColor"
+										id="yellowBG"
+										value="yellowBg"
+										onChange={this.handleChange}
+									/>
+									<label htmlFor="bgColor">Color</label>
+								</div>
 
-                {/* image */}
-                <div className="input-group non-text image-selector">
-                  <div className="options">
-                    <div className="img-radio">
-                      <input
-                        type="radio"
-                        name="bgPhoto"
-                        id="choice-1"
-                        value="/img/photo1.png"
-                        onChange={this.handleChange}
-                      />
-                      <img src="/img/photo1.png" name="photo1" alt="" />
-                    </div>
-                    <div className="img-radio">
-                      <input
-                        type="radio"
-                        name="bgPhoto"
-                        id="choice-2"
-                        value="/img/photo2.jpg"
-                        onChange={this.handleChange}
-                      />
-                      <img src="/img/photo2.jpg" name="photo2" alt="" />
-                    </div>
-                    <div className="img-radio">
-                      <input
-                        type="radio"
-                        name="bgPhoto"
-                        id="choice-3"
-                        value="/img/photo3.jpg"
-                        onChange={this.handleChange}
-                      />
-                      <img src="/img/photo3.jpg" name="photo3" alt="" />
-                    </div>
-                  </div>
+								{/* image */}
+								<div className="input-group non-text image-selector">
+									<div className="options">
+										<div className="img-radio">
+											<input
+												type="radio"
+												name="bgPhoto"
+												id="choice-1"
+												value="/img/photo1.png"
+												onChange={this.handleChange}
+											/>
+											<img src="/img/photo1.png" name="photo1" alt="" />
+										</div>
+										<div className="img-radio">
+											<input
+												type="radio"
+												name="bgPhoto"
+												id="choice-2"
+												value="/img/photo2.jpg"
+												onChange={this.handleChange}
+											/>
+											<img src="/img/photo2.jpg" name="photo2" alt="" />
+										</div>
+										<div className="img-radio">
+											<input
+												type="radio"
+												name="bgPhoto"
+												id="choice-3"
+												value="/img/photo3.jpg"
+												onChange={this.handleChange}
+											/>
+											<img src="/img/photo3.jpg" name="photo3" alt="" />
+										</div>
+									</div>
 
-                  <label htmlFor="bgPhoto">Image</label>
-                </div>
+									<label htmlFor="bgPhoto">Image</label>
+								</div>
 
-                <input type="submit" name="Generate!" />
-              </form>
-            </div>
-            <div className="right-group">
-              {this.state.showGeneratedImage ? (
-                <ImageGenerator
-                  name={this.state.name}
-                  location={this.state.location}
-                  file={this.state.file}
-                  blurb={this.state.blurb}
-                  bgColor={this.state.bgColor}
-                  bgPhoto={this.state.bgPhoto}
-                />
-              ) : (
-                <img className="example" src="/img/example.png" />
-              )}
+								<input type="submit" name="Generate!" />
+							</form>
+						</div>
+						<div className="right-group">
+							{this.state.showGeneratedImage ? (
+								<ImageGenerator
+									name={this.state.name}
+									location={this.state.location}
+									file={this.state.file}
+									blurb={this.state.blurb}
+									bgColor={this.state.bgColor}
+									bgPhoto={this.state.bgPhoto}
+								/>
+							) : (
+								<img className="example" src="/img/example.png" alt="example" />
+							)}
 
-              {/* social */}
-              <div className="social">
-                <a href="https://twitter.com/Booker4KY" taget="_blank">
-                  <TwitterIcon />
-                </a>
-                <a href="https://www.facebook.com/Booker4KY/" taget="_blank">
-                  <FacebookIcon />
-                </a>
-                <a href="https://www.instagram.com/booker4ky" taget="_blank">
-                  <InstagramIcon />
-                </a>
-                <p>
-                  If you like this, please follow me on social media — and say
-                  hello! And when you share your image include the hashtag{' '}
-                  <span className="bold">#IEndorseBooker</span>.
-                </p>
-              </div>
-            </div>
-          </article>
-        </section>
-      </div>
-    );
-  }
+							{/* social */}
+							<div className="social">
+								<a href="https://twitter.com/Booker4KY" taget="_blank">
+									<TwitterIcon />
+								</a>
+								<a href="https://www.facebook.com/Booker4KY/" taget="_blank">
+									<FacebookIcon />
+								</a>
+								<a href="https://www.instagram.com/booker4ky" taget="_blank">
+									<InstagramIcon />
+								</a>
+								<p>
+									If you like this, please follow me on social media — and say
+									hello! And when you share your image include the hashtag{' '}
+									<span className="bold">#IEndorseBooker</span>.
+								</p>
+							</div>
+						</div>
+					</article>
+				</section>
+			</div>
+		);
+	}
 }
 
 export default App;
