90b3c1b9b68a21ad077b36bccdc61a1dd4ae6f6b
Jordan Davis <jordan@milkshake.studio>
2020-05-26 20:29:13 -0400

    fixed for safari

--- src/ImageGenerator.js
+++ src/ImageGenerator.js
@@ -17,7 +17,7 @@ export default function ImageGenerator(props) {
       <div className="text">
         {props.location && <h3 className="location">{props.location}</h3>}
         <h2 className="endorsement">{props.name}</h2>
-		  <h2 className="endorsement"> <i>endorses</i> Booker</h2>
+        <h2 className="endorsement"> <i>endorses</i> Booker</h2>
         <h3 className="blurb">{props.blurb}”</h3>
       </div>
 
@@ -28,7 +28,7 @@ export default function ImageGenerator(props) {
 
       {/* background color */}
       <div className="background-color"></div>
-      
+
       {/* color overlay */}
       <div className="overlay-color"></div>
     </div>
